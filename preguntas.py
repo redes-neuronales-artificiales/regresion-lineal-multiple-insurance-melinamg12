1a1dbd34428f71ab7661af0fb658696e8b58d9e4
melinamg12 <memunozga@unal.edu.co>
2022-06-27 04:01:55 +0000

    Intento 2

--- preguntas.py
+++ preguntas.py
@@ -109,7 +109,7 @@ def pregunta_03():
     )
 
     # Cargua de las variables.
-    X_train, _, y_train, _ = pregunta_02()
+    X_train, X_test, y_train, y_test = pregunta_02()
 
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
